23921ec2ecd7ab6722cb1f9678df61df772f2f96
Thomas Jolain <thomas.jolain@gmail.com>
2015-01-14 21:30:16 +0100

    Added print string functionality
    Added reading disk functionality

--- boot.asm
+++ boot.asm
@@ -1,8 +1,7 @@
 ;	
 ;	Boot sector test to print a custom string... just for testing purpose.
 ;	
-%include "string_references.asm"
-%include "functions.asm"
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;	MAIN
@@ -12,15 +11,18 @@
 									;	In this case we references the bootloader reserved zone
 
 test_1:									
-	mov bx, HELLO_WORLD				;	use bx as parameter
-	call print							;	call the print functions
 
-	mov bx, SHUTDOWN					;	use bx as parameter
-	call print							;	call the print function again
-	
-	jmp test_1
+mov si, HELLO_WORLD		;	store string in SI
+call printstr						;	print it
+
+call readFloppyDrive
+
+
 
 jmp $								;	Hang
+
+%include "string_references.asm"
+%include "functions.asm"
 
 times 510-($-$$) db 0				;	Padding rest of software
 dw 0xaa55							;	Insert bootloader signature
--- functions.asm
+++ functions.asm
@@ -1,20 +1,41 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;	String References
+;	Functions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-print:
-    next_character:
-        mov al, [si]
-        inc si
-        or al, al
-        jz exit_function
-        call PrintCharacter
-        jmp next_character
-    exit_function:
-        ret
+printstr:
+	next_char:
+		mov al, [SI]							;	Before launching this, ensure that ASCII value is in AL
+													;	The string reference, or offset, must be stored in SI
+		inc SI									;	Increment SI Pointer
+		or AL, AL								;	Check if AL == 0
+		jz exit_printstr						;	If we encounter '0', it means that we arrived at the end of the string and must end the treatement
+		call printChar						;	Print a character
+		jmp next_char						;
+	exit_printstr:								;
+		ret										;
 		
-PrintCharacter:
-    mov ah, 0x0E
-    mov bh, 0x00
-    mov bl, 0x07
-    int 0x10
-    ret
+printChar:
+	mov ah, 0x0e		;	Set to write one character mode on BIOS
+	mov bh, 0x00		;	Set page N°
+	mov bl, 0x0A		;	Set color to green on black
+	
+	int 0x10				;	Execute "PRINT"
+	ret						;	return
+	
+readFloppyDrive:
+	mov ah, 0x02		;	Set Bios to READ DISK
+	mov dl,0x00			;	Set DISK to 0
+	mov ch, 0x03		;	Select Cylinder 3
+	mov dh, 0x00		;	Read recto of floppy. Verso is 0x01
+	mov cl, 0x04			;	Select the fourth sector
+	mov al, 0x05		;	Read 5 sectors from the start point
+	
+								;	Once parametrized, let's read it to some point
+								;	We must specify an address to store data
+	
+	mov bx, 0xa000	;	Set segment ES to 0xa000
+	mov es, bx			;	Prepare first address of segment
+	mov bx, 0x1234	;	Set endpoint to 0x1234
+	
+								;	This means we're going to read 5 sectors and store them from 0xa000 to 0x1234
+								
+	int 0x13				;	Interrupt
