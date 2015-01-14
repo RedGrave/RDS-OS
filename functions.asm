c743ee2d7a91e461a17f151902abc057c4504446
Thomas Jolain <thomas.jolain@gmail.com>
2015-01-14 01:36:31 +0100

    changed printing function

--- functions.asm
+++ functions.asm
@@ -2,6 +2,19 @@
 ;	String References
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 print:
-	mov ah, 0x0e
-	int 0x10
-	ret
+    next_character:
+        mov al, [si]
+        inc si
+        or al, al
+        jz exit_function
+        call PrintCharacter
+        jmp next_character
+    exit_function:
+        ret
+		
+PrintCharacter:
+    mov ah, 0x0E
+    mov bh, 0x00
+    mov bl, 0x07
+    int 0x10
+    ret
